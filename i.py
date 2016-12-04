b86057eca09a38d796471a65255cbdd34d782a5e
dormitory <example@izding.com>
2016-12-04 23:25:41 +0800

    第五章内容小部分更新

--- i.py
+++ i.py
@@ -1,11 +1,11 @@
-def odd():
-    print('step 1')
-    yield 1
-    print('step 2')
-    yield(3)
-    print('step 3')
-    yield(5)
-o = odd();
-print('执行第一次',next(o))
-print('执行第二次',next(o))
-print('执行第三次',next(o))    
+def log(func):
+    def wrapper(*a,**b):
+        print('call %s()' % func.__name__)
+        return func(*a,**b)
+    return wrapper
+
+@log
+def r():
+    print('???')
+
+r()
